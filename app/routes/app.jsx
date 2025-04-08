a69c10d7b57da1362e628d4c5e72a020ede89024
leonli <leonli@aaxis.io>
2025-04-08 19:08:36 +0800

    replace

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,9 +1,8 @@
-import React from "react";
-import { json } from "@remix-run/node";
 import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
-import { AppProvider as PolarisAppProvider } from "@shopify/polaris";
-import polarisStyles from "@shopify/polaris/build/esm/styles.css";
-import { boundary } from "@shopify/shopify-app-remix";
+import { boundary } from "@shopify/shopify-app-remix/server";
+import { AppProvider } from "@shopify/shopify-app-remix/react";
+import { NavMenu } from "@shopify/app-bridge-react";
+import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
 
 import { authenticate } from "../shopify.server";
 
@@ -12,33 +11,27 @@ export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
 
-  return json({
-    polarisTranslations: require("@shopify/polaris/locales/en.json"),
-    apiKey: process.env.SHOPIFY_API_KEY,
-  });
-}
+  return { apiKey: process.env.SHOPIFY_API_KEY || "" };
+};
 
 export default function App() {
-  const { polarisTranslations } = useLoaderData();
+  const { apiKey } = useLoaderData<typeof loader>();
 
   return (
-    <>
-      <PolarisAppProvider
-        i18n={polarisTranslations}
-        linkComponent={RemixPolarisLink}
-      >
-        <Outlet />
-      </PolarisAppProvider>
-    </>
+    <AppProvider isEmbeddedApp apiKey={apiKey}>
+      <NavMenu>
+        <Link to="/app" rel="home">
+          Home
+        </Link>
+        <Link to="/app/company-role-management">Company Role Management</Link>
+        <Link to="/app/customer-part-number">Customer Part Number</Link>
+        <Link to="/app/quotes">Quotes</Link>
+        <Link to="/app/po-automation">PO Automation</Link>
+      </NavMenu>
+      <Outlet />
+    </AppProvider>
   );
 }
-
-/** @type {any} */
-const RemixPolarisLink = React.forwardRef((/** @type {any} */ props, ref) => (
-  <Link {...props} to={props.url ?? props.to} ref={ref}>
-    {props.children}
-  </Link>
-));
 
 // Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
 export function ErrorBoundary() {
