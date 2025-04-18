945749e7d2b5e0db8e1f9f2051d63aff197c20c3
leonli <leonli@aaxis.io>
2025-04-18 15:55:02 +0800

    replace

--- app/routes/app.test.tsx
+++ app/routes/app.test.tsx
@@ -3,6 +3,22 @@ import { Card } from "~/components/ui/card";
 import { useState } from "react";
 import { BILLING_CONFIG, getBillingMutationVariables } from "~/config/billing";
 import { CREATE_ONE_TIME_PURCHASE, CREATE_SUBSCRIPTION } from "~/graphql/mutations/billing";
+import type {LoaderFunctionArgs} from '@remix-run/node';
+import {authenticate} from '~/shopify.server';
+
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  console.log("/app._index:loader");
+  const apiKey = process.env.SHOPIFY_API_KEY;
+  try {
+    const { session } = await authenticate.admin(request);
+    const text = { shopDomain: session.shop, apiKey: apiKey };
+    return text;
+  } catch (error) {
+    console.log("app loader error", error);
+    return null
+  }
+};
 
 export default function BillingPage() {
 
